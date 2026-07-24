2f048d186ae17a3e3ac250410d5f500f151c9a14
caedmonp <82240453+caedmonpowell@users.noreply.github.com>
2026-07-24 10:34:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const cube = document.getElementById("cube");
 
 let rotation = 0;
+let last = performance.now();
 
-function animate(){
+function animate(now) {
 
-    rotation += 0.02;
+    const delta = now - last;
+    last = now;
+
+    rotation += delta * 0.0018;
 
     cube.style.transform = `
         translate(-50%, -50%)
         rotateX(-10deg)
         rotateY(${rotation}deg)
-        scale(1.35)
+        scale(1.38)
     `;
 
     requestAnimationFrame(animate);
 
 }
 
-animate();
+requestAnimationFrame(animate);
