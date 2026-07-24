d5c1220cc1eb06bbbfaae3924bfb0b909b7d6340
caedmonp <82240453+caedmonpowell@users.noreply.github.com>
2026-07-24 04:46:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Website loaded!");
+const cube = document.getElementById("cube");
+
+
+let rotation = 0;
+
+
+function animate(){
+
+    rotation += 0.02;
+
+
+    cube.style.transform = `
+
+        translate(-50%, -50%)
+
+        rotateX(-10deg)
+
+        rotateY(${rotation}deg)
+
+        scale(1.35)
+
+    `;
+
+
+    requestAnimationFrame(animate);
+
+}
+
+
+animate();
